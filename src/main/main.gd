02c461d950eb61866b02f374edd89adaf04896cb
DocDamage <dsferraro@gmail.com>
2026-08-11 08:14:21 -0400

    P01-T02 wire Home screen into app shell

--- src/main/main.gd
+++ src/main/main.gd
@@ -1,4 +1,18 @@
 extends Control
 
+signal route_requested(route: StringName)
+
+const ThemeFactory = preload("res://src/app/theme/theme_factory.gd")
+
+@onready var home_screen: Control = $HomeScreen
+
+
 func _ready() -> void:
-    print("PlayWorld Studio starter scaffold loaded.")
+    theme = ThemeFactory.create_theme()
+    home_screen.route_requested.connect(_on_home_route_requested)
+    print("PlayWorld Studio application shell loaded.")
+
+
+func _on_home_route_requested(route: StringName) -> void:
+    route_requested.emit(route)
+    print("PlayWorld route requested: %s" % route)
